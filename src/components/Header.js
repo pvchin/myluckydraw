1932b25ef57d2a3c1ea49b7d91b33f3c55b44af2
pvchine <pvchinbn@yahoo.com>
2022-12-14 20:23:07 +0800

    Dec 14 22F

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,46 +22,8 @@ const Header = () => {
     <Flex minH={50}>
       <Grid templateColumns="repeat(12, 1fr)" gap={0}>
         <GridItem colSpan={12}>
-          <Image src={bannerimg} alt="banner" />
           <Center>
-            <Box position="absolute" top="150" left="90">
-              {/* <MovingComponent
-                type="flash"
-                duration="1000ms"
-                delay="0s"
-                direction="normal"
-                timing="ease"
-                iteration="5"
-                fillMode="none"
-              >
-                <Image src={img1} w={200} />
-              </MovingComponent> */}
-            </Box>
-            <Box position="absolute" top="330" left="710">
-              {/* <MovingComponent
-                type="fadeInFromLeft"
-                duration="1000ms"
-                delay="0s"
-                direction="normal"
-                timing="ease"
-                iteration="5"
-                fillMode="none"
-              >
-                <Heading color="black" fontFamily="serif" size="lg">
-                  One Winner will be announced on 1/1/2023
-                </Heading>
-              </MovingComponent> */}
-            </Box>
-            {/* <MovingComponent
-              type="flash"
-              duration="1000ms"
-              delay="0s"
-              direction="normal"
-              timing="ease"
-              iteration="5"
-              fillMode="none"
-            ></MovingComponent> */}
-            {/* <Heading fontWeight={2}>Giveaway Lucky Draw</Heading> */}
+            <Image src={bannerimg} alt="banner" />
           </Center>
         </GridItem>
         {/* <GridItem colSpan={1}>
